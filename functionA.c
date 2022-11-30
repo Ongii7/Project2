fe6d792537f91c30b30967695d06dabe6452f2c5
An Le <anle1@cpp.edu>
2022-11-29 22:14:06 -0800

    Created getAmountPerMilesDriven function

--- functionA.c
+++ functionA.c
@@ -1,23 +1,24 @@
-int inputCheckInt() {
-    int i, input;
-    do {
-        i = scanf("%d", &input);
-        if (i != 1) {
-            printf("Invalid input!\n: ");
-            scanf("%*s");
-            // clear input buffer
-            while (getchar() != '\n')
-                ;
-        }
-    } while (i != 1);
-    return input;
-};
+// int inputCheckInt() {
+//     int i, input;
+//     do {
+//         i = scanf("%d", &input);
+//         if (i != 1) {
+//             printf("Invalid input!\n: ");
+//             scanf("%*s");
+//             // clear input buffer
+//             while (getchar() != '\n')
+//                 ;
+//         }
+//     } while (i != 1);
+//     return input;
+// };
 
 void getTotDaysOnTrip(int* totalDate) {
     printf("Please enter number of day on the trip:");
     do {
-        *totalDate = inputCheckInt();
-        if (totalDate < 1) {
+        // *totalDate = inputCheckInt();
+        scanf("%d", totalDate);
+        if (*totalDate < 1) {
             printf("Please enter at least 1 day or more!\n: ");
         } else {
             break;
@@ -45,4 +46,15 @@ void getDepartureAndArriveTime(double* departure, double* arrival) {
     *departure = inputTimeValidation();
     printf("Please enter arrival time: ");
     *arrival = inputTimeValidation();
+};
+
+void getAmountPerMilesDriven(double* totalExpense) {
+    double miles;
+    printf("Is it a private vehicle? y/n");
+    char v = inputValidate2();
+    if (v == 'Y' || v == 'y') {
+        printf("Please enter # of miles driven: ");
+        scanf("%f", &miles);
+        *totalExpense = *totalExpense + (miles*0.27);
+    }
 };
