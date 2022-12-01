f39c0b3d98bf7d47d5df432b98734506b3720cbd
Ongii7 <ongii.k18@gmail.com>
2022-11-30 18:29:48 -0800

    fixed variable names

--- expenses.h
+++ expenses.h
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <stddef.h>
 
-int tot_days_on_trip; //# of days spent on trip
+int tot_day_on_trip; //# of days spent on trip
 double depart_time; //departure time
 double arrival_time; //arrival time
 double tot_expense;
 double tot_allowable_expense;
 double tot_reimbursement;
-double tot_amount_saved;
+double tot_amount_save;
--- main.c
+++ main.c
@@ -4,10 +4,10 @@
 
 int main(void) {
     //call total number of days spent on the trip
-    getTotDaysOnTrip(tot_days_on_trip);
+    getTotDaysOnTrip(tot_day_on_trip);
 
     //call time of departure on first day of trip and arrival back home of last day of trip
-    getDepartAndArrivalTime(depart_time, arrival_time);
+    getDepartureAndArrivalTime(depart_time, arrival_time);
 
     //amount of any round-trip airfare
     getRoundTripAirFare(tot_expense);
@@ -19,21 +19,21 @@ int main(void) {
     getPrivateVehicleUse(tot_expense);
 
     //parking fees (company allows up to $6 a day)
-    getParkingFee(tot_expense, tot_days_on_trip, tot_allowable_expense, tot_reimbursement, tot_amount_saved);
+    getParkingFee(tot_expense, tot_day_on_trip, tot_allowable_expense, tot_reimbursement, tot_amount_save);
 
     //taxi fees if any taxi was used during the trip (allows up to $10 a day)
-    getTaxiFee(tot_expense, tot_days_on_trip, tot_allowable_expense, tot_reimbursement, tot_amount_saved);
+    getTaxiFee(tot_expense, tot_day_on_trip, tot_allowable_expense, tot_reimbursement, tot_amount_save);
 
     //conference or seminar registration fees
     getConferenceAndRegistrateFee(tot_expense);
 
     //hotel expenses (allows up to $90 a day)
-    getHotelFee(tot_expense, tot_days_on_trip, tot_allowable_expense, tot_reimbursement, tot_amount_saved);
+    getHotelFee(tot_expense, tot_day_on_trip, tot_allowable_expense, tot_reimbursement, tot_amount_save);
 
     //the amount of each meal eaten
-    getMealFee(tot_expense, depart_time, tot_allowable_expense, tot_reimbursement, tot_amount_saved, arrival_time);
+    getMealFee(tot_expense, depart_time, tot_allowable_expense, tot_reimbursement, tot_amount_save, arrival_time);
 
-    displayTot(tot_expense, tot_allowable_expense, tot_reimbursement, tot_amount_saved);
+    displayTot(tot_expense, tot_allowable_expense, tot_reimbursement, tot_amount_save);
 
     /**
      * display:
